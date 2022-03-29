8f4bd555bc7e8b59f125f7a4af93818bf995d57b
SirADV24 <vranciuadrian24@gmail.com>
2022-03-29 09:57:54 +0300

    TESTS

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -1,20 +1,60 @@
 package ssvv.example;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ssvv.example.domain.Student;
+import ssvv.example.repository.NotaXMLRepo;
+import ssvv.example.repository.StudentXMLRepo;
+import ssvv.example.repository.TemaXMLRepo;
+import ssvv.example.service.Service;
+import ssvv.example.validation.NotaValidator;
+import ssvv.example.validation.StudentValidator;
+import ssvv.example.validation.TemaValidator;
 
-import org.junit.Test;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    private Service service;
+
+    @BeforeEach
+    public void setUp() {
+        String filenameStudent = "fisiere_test/Studenti.xml";
+        String filenameTema = "fisiere_test/Teme.xml";
+        String filenameNota = "fisiere_test/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+
+        this.service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
     }
+
+    @AfterEach
+    public void cleanup() throws IOException {
+        // Cleanup files after each test run to keep validity of i/o operations
+        Path file = Paths.get("fisiere_test/Studenti.xml");
+        Files.write(file, Collections.singletonList("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><inbox></inbox>"), StandardCharsets.UTF_8);
+        file = Paths.get("fisiere_test/Teme.xml");
+        Files.write(file, Collections.singletonList("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><inbox></inbox>"), StandardCharsets.UTF_8);
+        file = Paths.get("fisiere_test/Note.xml");
+        Files.write(file, Collections.singletonList("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><inbox></inbox>"), StandardCharsets.UTF_8);
+    }
+
+    @Test
+    public void test_1_addStudent_success(){
+        service.addStudent(new Student("1", "Real name", 1, "realemail@gmail.com"));
+    }
+
+
 }
